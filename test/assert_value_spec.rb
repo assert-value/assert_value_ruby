5a96531111c37e26c99c165fd03f0000c1a8a790
Alexander Dymo <adymo@eligoenergy.com>
2013-10-24 17:02:47 -0500

    Demonstrate how inlined values to assert_value can be used in rspec

--- test/assert_value_spec.rb
+++ test/assert_value_spec.rb
@@ -6,8 +6,14 @@ end
 
 describe "Assert Value" do
 
-  it "compares with log in files" do
+  it "compares with value in file" do
     expect(assert_value("foo", :log => 'test/logs/assert_value_with_files.log.ref')).to eq(true)
+  end
+
+  it "compares with value inlined in source file" do
+    assert_value "foo", <<-END
+        foo
+    END
   end
 
 end
